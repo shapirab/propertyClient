d8232030bcc88f886f8b1719f4e7915a21b8929f
shapirab <shapirab@jamd.ac.il>
2024-11-06 08:52:33 +0200

    6/11/24 added router support to navbar component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { RentComponent } from './components/rent/rent.component';
+import { BuyComponent } from './components/buy/buy.component';
+import { ListPropertyComponent } from './components/list-property/list-property.component';
 
 const routes: Routes = [
-  {path: 'home', component: HomeComponent},
+  {path: 'home',
+    component: HomeComponent,
+    children: [
+      { path: 'rent', component: RentComponent },
+      { path: 'buy', component: BuyComponent },
+      { path: 'list-property', component: ListPropertyComponent }
+    ]
+  },
   {path: '', redirectTo: 'home', pathMatch: 'full'}
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { BuyComponent } from './components/buy/buy.component';
+import { RentComponent } from './components/rent/rent.component';
+import { ListPropertyComponent } from './components/list-property/list-property.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    NavbarComponent
+    NavbarComponent,
+    BuyComponent,
+    RentComponent,
+    ListPropertyComponent
   ],
   imports: [
     BrowserModule,
